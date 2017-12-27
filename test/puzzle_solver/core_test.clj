345fabd75e793e303b863068c0f91fde27f67266
Juhani Hietikko <juhani.hietikko@reaktor.com>
2017-12-27 22:22:20 +0200

    Initial commit

---
+++ test/puzzle_solver/core_test.clj
@@ -0,0 +1,7 @@
+(ns puzzle-solver.core-test
+  (:require [clojure.test :refer :all]
+            [puzzle-solver.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
