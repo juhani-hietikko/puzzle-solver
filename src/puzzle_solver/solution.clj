d69348a9d5635be64ed30160df18a12cc40cf622
Juhani Hietikko <juhani.hietikko@reaktor.com>
2017-12-28 21:17:51 +0200

    Probably working solver but slow as hell

--- src/puzzle_solver/solution.clj
+++ src/puzzle_solver/solution.clj
@@ -1,9 +1,24 @@
 (ns puzzle-solver.solution
   (:require [puzzle-solver.pieces :refer [piece-configs]]
-            [puzzle-solver.board :refer [empty-board]]))
+            [puzzle-solver.board :refer [empty-board
+                                         possible-placements
+                                         place]]))
 
 (defn- branch [placed remaining board]
-  )
+  (swap! n inc)
+  (if (empty? remaining)
+    placed
+    (let [piece (first remaining)
+          possibilities (possible-placements board piece)]
+      (some
+        #(branch
+           (cons {:piece  piece
+                  :coords %}
+                 placed)
+           (rest remaining)
+           (place board piece %))
+        possibilities))))
 
 (defn solutions []
-  )
+  ;(prn (count piece-configs))
+  (branch '() (first piece-configs) empty-board))
