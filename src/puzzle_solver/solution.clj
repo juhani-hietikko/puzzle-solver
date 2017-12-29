6cc28115b2d7f11755d72a7ddc61a9bab5706283
Juhani Hietikko <juhani.hietikko@reaktor.com>
2017-12-29 12:06:26 +0200

    Flatten solutions

--- src/puzzle_solver/solution.clj
+++ src/puzzle_solver/solution.clj
@@ -9,11 +9,9 @@
 (defn- branch [placed remaining board]
   (swap! n inc)
   (cond
-    #_(empty? remaining)
-    (< (count remaining) 5)
-    (do (prn "HUHUHUU")
-        (prn placed)
-        placed)
+    #_(< (count remaining) 6)
+    (empty? remaining)
+    {:placements placed}
     (hopeless? board)
     nil
     #_(when (= 0 (mod @n 500))
@@ -28,10 +26,19 @@
                                     placed)
                               (rest remaining)
                               (place board piece %)))
-                         (filter identity))]
+                         (filter identity)
+                         flatten)]
       (when (seq solutions)
         solutions))))
 
 (defn solutions []
   ;(prn (count piece-configs))
   (branch '() (first piece-configs) empty-board))
+
+
+#_(count (->> (take 1000 piece-configs)
+              (filter (fn [pc]
+                        (= 30 (count (->> pc
+                                          (mapcat parts)
+                                          (map #(nth % 2))
+                                          (filter #(= :- %)))))))))
